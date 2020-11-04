f3d7f4de01c396e4e5ac906768aca993b11ff6e3
sjoerdvgerwen <63058506+sjoerdvgerwen@users.noreply.github.com>
2020-11-04 13:48:20 +0100

    Nieuwe project aangemaakt
    Begrepen hoe de URL routing werkt
    Entity Framework toegepast na heel veel pogingen

---
+++ WarehouseManagement1/WarehouseManagement1/Controllers/StockController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WarehouseManagement1.Models;
+
+namespace WarehouseManagement1.Controllers
+{
+    public class StockController : Controller
+    {
+        public IActionResult StockList()
+        {
+            var stock = new Stock() { Name = "Moersleutel", Id = 1 };
+
+            return View(stock); 
+        }
+    }
+}
---
+++ WarehouseManagement1/WarehouseManagement1/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarehouseManagement1.Models;
+
+namespace WarehouseManagement1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Stock> Stocks { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+    }
+}
