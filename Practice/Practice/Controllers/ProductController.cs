4f7330debae0cda15ce0a3537d5b219a1e7bae25
sjoerdvgerwen <63058506+sjoerdvgerwen@users.noreply.github.com>
2020-11-11 13:07:22 +0100

    Lagenstructuur gemaakt + Database verbinding (Selfservice portal)

---
+++ Practice/Practice/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Webapp.Models;
+using Warehouse.Application.Entity;
+using Warehouse.Application.Interfaces;
+
+namespace Warehouse.Webapp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        //Connectie van View Controller naar Application Interfaces
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AddProduct(ProductViewModel product) 
+        {
+            Product newProduct = new Product()
+            {
+                ProductID = Guid.NewGuid(),
+                ProductName = product.ProductName,
+                ProductDescription = product.ProductDescription
+
+                
+            };
+
+            return Ok(await _productRepository.AddProduct(newProduct));
+                
+        }
+    }
+}
---
+++ Practice/Practice/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Practice.Models;
+
+namespace Practice.Controllers
+
+    
+{
+    public class UserController : Controller
+    {
+        public User user;
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            return View();
+        }
+    }
+}
